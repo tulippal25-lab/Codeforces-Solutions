8cef2a870f28f5ef8e1a069f9bb27c5f0d00285b
TulipPal <tulippal25@gmail.com>
2026-07-27 22:44:32 +0530

    Added Good Times solution

---
+++ GoodTimes.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int x = sc.nextInt();
+
+            int y = 1;
+
+            while (x > 0) {
+                y *= 10;
+                x /= 10;
+            }
+
+            System.out.println(y + 1);
+        }
+
+        sc.close();
+    }
+}
