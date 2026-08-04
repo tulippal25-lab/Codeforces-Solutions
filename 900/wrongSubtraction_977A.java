20d73a27412f2eaa861227092dd28117939ab09f
TulipPal <tulippal25@gmail.com>
2026-08-04 23:27:32 +0530

    997A solution

---
+++ 900/wrongSubtraction_977A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class wrongSubtraction_977A {
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        while (k > 0) {
+            if (n % 10 == 0) {
+                n /= 10;
+            }
+            else{
+                n -= 1;
+            }
+            k--;
+        }
+        System.out.println(n);
+    }
+}
