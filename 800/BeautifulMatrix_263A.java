f99240062a68b6bf57724f6210e989881e5e8905
tulippal25-lab <tulippal25@gmail.com>
2026-08-23 21:28:46 +0530

    Add 263A solution

---
+++ 800/BeautifulMatrix_263A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BeautifulMatrix_263A {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int x = sc.nextInt();
+
+                if (x == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        int moves = Math.abs(row - 2) + Math.abs(col - 2);
+        System.out.println(moves);
+
+    }
+}
