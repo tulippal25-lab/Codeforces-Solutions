d6f368f3f590e67b71e706d30c7ccb1a70c0495e
tulippal25-lab <tulippal25@gmail.com>
2026-08-24 17:51:40 +0530

    Add 41A solution

---
+++ 800/Translation_41A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Translation_41A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        if (reversed.equals(t)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
