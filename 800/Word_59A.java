a23686ac04264870aa660a74a4febf0a41458ff4
tulippal25-lab <tulippal25@gmail.com>
2026-08-24 17:40:50 +0530

    Add 59A solution

---
+++ 800/Word_59A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Word_59A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int upper = 0, lower = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                upper++;
+            } else {
+                lower++;
+            }
+        }
+
+        if (upper > lower) {
+            System.out.println(s.toUpperCase());
+        } else {
+            System.out.println(s.toLowerCase());
+        }
+    }
+}
